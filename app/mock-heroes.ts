468325ee2240212cb9c7d0fcaa4e165f69a11814
tatianaflo <tatianaflo@hotmail.fr>
2016-12-07 23:21:29 +0100

    7 décembre bis

---
+++ app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES:  Hero[] = [
+  {id: 1,     name : 'Wonder Woman'},
+  {id: 2,     name : 'SuperGirl'},
+  {id: 3,     name : 'BatGirl'},
+  {id: 4,     name : 'Harley Quinn'},
+  {id: 5,     name : 'HawkGirl'},
+  {id: 6,     name : 'Poison Evil'},
+  {id: 7,     name : 'Bumble Bee'},
+  {id: 8,     name : 'Katana'},
+  {id: 9,     name : 'Barry Allen'},
+  {id: 10,    name : 'Hal Jordan'}
+];
